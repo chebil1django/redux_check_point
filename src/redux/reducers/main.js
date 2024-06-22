08b2c21c23712b8878e0bf8a36b20a0e28e31f0e
med_django <59399349+chebil1django@users.noreply.github.com>
2024-06-22 07:11:48 +0100

    Delete src directory

--- src/redux/reducers/main.js
+++
@@ -1,8 +0,0 @@
-import { todoreducers } from "./reducer";
-import { combineReducers } from "redux";
-
-const rootreducers = combineReducers({
-  todoreducers,
-});
-
-export default rootreducers;
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import rootreducers from "./redux/reducers/main";
-
-
-const store = createStore(
-    rootreducers
-);
-
-export default store;
